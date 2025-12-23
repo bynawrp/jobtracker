1f89f947794d751ca3fa23b951aa2b60f289de1a
bynawrp <jeanlucas84@outlook.com>
2025-12-23 15:11:45 +0100

    fix: api url

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,8 +1,8 @@
 // API
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL 
-    ? `${import.meta.env.VITE_API_URL}/api` 
+const API_BASE_URL = import.meta.env.API_URL 
+    ? `${import.meta.env.API_URL}/api` 
     : '/api';
 
 const api = axios.create({
